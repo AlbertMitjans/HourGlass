Fixed errors and added gradient values to output saved imgs

my_classes.py
@@ -86,9 +86,6 @@ class CornersDataset(Dataset):
             image = Image.open(img_name)
             image = transforms.ToTensor()(image).type(torch.float32)
             image = image / image.max()
-            edges = transforms.ToTensor()(transforms.ToPILImage()(image[0]).convert('L').filter(ImageFilter.FIND_EDGES))
-            contours = transforms.ToTensor()(transforms.ToPILImage()(image[0]).convert('L').filter(ImageFilter.CONTOUR))
-            image = torch.stack((image[0], image[0], image[0]))
 
         grid = transforms.ToTensor()(gaussian(image[0], corners, target_size=image[0].size())[0]).type(torch.float32)
 
@@ -97,6 +94,13 @@ class CornersDataset(Dataset):
         if not self.validation:
             if self.transform:
                 sample = self.transform(sample)
+
+        if self.depth:
+            image = sample['image']
+            edges = transforms.ToTensor()(transforms.ToPILImage()(image[0]).convert('L').filter(ImageFilter.FIND_EDGES))
+            contours = transforms.ToTensor()(transforms.ToPILImage()(image[0]).convert('L').filter(ImageFilter.CONTOUR))
+            image = torch.stack((image[0], edges[0], image[0]))
+            sample['image'] = image
 
         sample['image'] = pad_to_square(sample['image'])
         sample['grid'] = pad_to_square(sample['grid'])
@@ -150,18 +154,15 @@ class RandomCrop(object):
 
     def __call__(self, sample):
         if np.random.random() < 0.5:
-            image, grid = sample['image'], sample['grid']
+            image, grid = sample['image'], sample['grid']/sample['grid'].max()
             rc = transforms.RandomCrop((int(image.shape[1] * self.size), int(image.shape[2] * self.size)))
             im_grid = torch.stack((image[0], grid[0]))
             crop_im_grid = rc(transforms.ToPILImage()(im_grid))
             crop_image, crop_grid = Image.Image.split(crop_im_grid)
             res_image = crop_image.resize((image.shape[2], image.shape[1]))
             grid = transforms.ToTensor()(crop_grid.resize((image.shape[2], image.shape[1])))
-            edges = transforms.ToTensor()(res_image.convert('L').filter(ImageFilter.FIND_EDGES))
-            contours = transforms.ToTensor()(res_image.convert('L').filter(ImageFilter.CONTOUR))
-            image = torch.stack((transforms.ToTensor()(res_image)[0], edges[0], contours[0]))
 
-            sample['image'] = image
+            sample['image'] = transforms.ToTensor()(res_image)
             sample['grid'] = grid
 
         return sample

train.py
@@ -11,7 +11,7 @@ from my_classes import init_model_and_dataset, adjust_learning_rate, AverageMete
 from utils.logger import Logger
 
 
-def train(ckpt, freeze, depth, normalize_data, end_epoch, single_grid):
+def train(ckpt, freeze, depth, normalize_data, end_epoch):
     batch_size = 1
     num_workers = 0
     lr = 5e-4
@@ -31,6 +31,8 @@ def train(ckpt, freeze, depth, normalize_data, end_epoch, single_grid):
     model, train_dataset, val_dataset, criterion_grid, optimizer = init_model_and_dataset(depth, directory,
                                                                                           normalize_data, lr,
                                                                                           weight_decay, momentum)
+    val_dataset.evaluate()
+
     # load the pretrained network
     if ckpt is not None:
         checkpoint = torch.load(ckpt)
@@ -82,10 +84,6 @@ def train(ckpt, freeze, depth, normalize_data, end_epoch, single_grid):
             loss = criterion_grid(output, grid, 0)
 
             # measure accuracy and record loss
-            show = False
-            if data_idx * grid.size(0) % 50 == 0:
-                show = True
-
             accuracy(corners, output.data, grid, input, end_epoch, epoch, train_recall, train_precision, depth=depth)
             train_loss.update(loss.item())
 
@@ -100,12 +98,10 @@ def train(ckpt, freeze, depth, normalize_data, end_epoch, single_grid):
 
             if data_idx % print_freq == 0 and data_idx != 0:
                 print('Epoch: [{0}][{1}/{2}]\t'
-                # 'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                # 'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                       'Loss.avg: {loss.avg:.4f}\t'
                       'Recall(%): {top1:.3f}\t'
                       'Precision num. corners (%): ({top2:.3f}, {top3:.3f}, {top4:.3f}, {top5:.3f})\t'.format(
-                    epoch, data_idx, len(train_loader), batch_time=batch_time, data_time=data_time, loss=train_loss,
+                    epoch, data_idx, len(train_loader), loss=train_loss,
                     top1=train_recall.avg * 100, top2=train_precision[0].avg * 100, top3=train_precision[1].avg * 100,
                     top4=train_precision[2].avg * 100, top5=train_precision[3].avg * 100))
 
@@ -113,7 +109,7 @@ def train(ckpt, freeze, depth, normalize_data, end_epoch, single_grid):
             # evaluate on validation set
             print('Train set:  ')
 
-            t_recall, t_precision = validate(train_loader, model, 100, epoch)
+            t_recall, t_precision = validate(train_loader, model, end_epoch, epoch)
             print('Validation set:  ')
             e_recall, e_precision = validate(val_loader, model, end_epoch, epoch)
 